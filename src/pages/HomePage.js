99bc43e9a5e0a3dc575554285006a74a32ff6de2
Ivan <ivangricev25@gmail.com>
2024-06-13 08:22:17 +0300

    Added from container and form

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,22 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form, Button } from 'react-bootstrap'; 
 import './HomePage.css';
 
+
 const HomePage = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        surname: '',
+        email: '',
+        phone: '',
+        theme: '',
+        message: ''
+    });
+    
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value });
+    };
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        // накинуть логики
+    };
+    
     return (
-        <div className="home-page container text-light">
-            <div className='row start-block container-fluid'>
-                <div className='col justify-content-lg-start'>
-                    <h1><span class="gradient-text">Dive</span> Into The Depths <br/> Of <span class="gradient-text"> Virtual Reality</span></h1>
+        <div className="home-page text-light container">
+            <div className='row start-block'>
+                <div className='col-lg-6 order-lg-2'>
+                    <h1><span className="gradient-text">Dive</span> Into The Depths <br/> Of <span className="gradient-text"> Virtual Reality</span></h1>
                     <p className="intro-text">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore 
-                        nisl tincidunt eget. Lectus mauris eros in vitae .
+                        nisl tincidunt eget. Lectus mauris eros in vitae.
                     </p>
-                    <button className="filled-button" onClick={() => window.location.href='/about'}>
+                    <button className="btn filled-button" onClick={() => window.location.href='/about'}>
                         BUILD YOUR WORLD
                     </button>
                 </div>
-                <div className='col justify-content-lg-end d-flex'>
+                <div className='col-lg-6 order-lg-1 d-flex justify-content-end'>
                     <div className='start-img-block'>
                         <img className="start-img" src="./img/start-img.png" alt="Logo"/>                
                     </div>
@@ -55,15 +76,15 @@ const HomePage = () => {
                         <img className="about-img hide" src="./img/arrow.svg" alt="arrow" style={{width: '228px', height: '100px', paddingLeft:'10%'}}/>                
                     </h2>
                 </div>
-                <div className='col justify-content-lg-end d-flex '>
+                <div className='col justify-content-lg-end algin-items-center'>
                     <p>Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Feugiat  nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo  quis imperdiet massa tincidunt nunc. Viverra aliquet eget sit amet tellus. Ornare lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus vitae.</p>
                 </div>
             </div>
-            <div className='about-block row container-fluid mt-6'>
+            <div className='about-block row container-fluid d-flex mt-6'>
                 <div className='col justify-content-lg-start'>
                     <img className="about-img" src="./img/about-block.png" alt="howto"/>                
                 </div>
-                <div className='col justify-content-lg-end d-flex flex-column align-self-center'>
+                <div className='col justify-content-lg-end flex-column align-self-center'>
                     <h2 className='fw-bold'>
                         ABOUT
                     </h2>
@@ -71,7 +92,7 @@ const HomePage = () => {
                         HYDRA VR
                     </h2>
                     <p className='pt-4'>
-                    Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis rhoncus urna neque viverra justo. Vivamus at augue eget arcu dictum. Ultrices gravida dictum fusce ut placerat orci. Aenean et tortor at risus viverra adipiscing at in. Mattis aliquam faucibus purus in massa. Est placerat in egestas erat imperdiet sed. Consequat semper viverra nam libero justo laoreet sit amet. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Laoreet sit amet cursus sit amet. Vel eros donec ac odio tempor orci dapibus. Sem nulla pha retra diam sit amet nisl suscipit adipiscing bibendum. Leo a diam sollicitudi n tempor.
+                        Eget mi proin sed libero enim sed faucibus turpis. Nisl rhoncus mattis rhoncus urna neque viverra justo. Vivamus at augue eget arcu dictum. Ultrices gravida dictum fusce ut placerat orci. Aenean et tortor at risus viverra adipiscing at in. Mattis aliquam faucibus purus in massa. Est placerat in egestas erat imperdiet sed. Consequat semper viverra nam libero justo laoreet sit amet. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Laoreet sit amet cursus sit amet. Vel eros donec ac odio tempor orci dapibus. Sem nulla pha retra diam sit amet nisl suscipit adipiscing bibendum. Leo a diam sollicitudi n tempor.
                     </p>
                     <button className="filled-button mt-4 w-50" onClick={() => window.location.href='/how-to'}>
                         LET’S GET IN TOUCH
@@ -88,11 +109,11 @@ const HomePage = () => {
                         <img className="about-img hide" src="./img/arrow.svg" alt="arrow" style={{width: '228px', height: '100px', paddingLeft:'10%'}}/>                
                     </h2>
                 </div>
-                <div className='col justify-content-lg-end d-flex '>
+                <div className='col justify-content-lg-end d-flex algin-items-center'>
                     <p>Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Feugiat  nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo  quis imperdiet massa tincidunt nunc. Viverra aliquet eget sit amet tellus. Ornare lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus vitae.</p>
                 </div>
             </div>
-            <div className='cards-block row container-fluid mt-6'>
+            <div className='cards-block row container-fluid mt-6 hide'>
                 <div className='card col text-light d-flex'>
                     <div className='align-self-center mt-3'>
                         <img className="card-img" src="./img/card1.png" alt="card1" style={{width: '228px', height: '100px'}}/>
@@ -138,7 +159,7 @@ const HomePage = () => {
                     </button>
                 </div>
             </div>
-            <div className='tecnologis-block container-fluid mt-6'>
+            <div className='tecnologis-block container-fluid mt-6 hide-tec'>
                 <div class="row tecnologi-main-img justify-content-lg-center align-items-center">
                     <div class="col text-center">
                         <h2 className='fs-1'>TECHNOLOGIES & HARDWARE</h2>
@@ -159,6 +180,110 @@ const HomePage = () => {
                         <img className="tecnologis-img" src="./img/vive.png" alt="tecnologis-main" style={{width: '100%', height: '100%'}}/>
                     </div>
                 </div>
+            </div>
+            <div className='row container-fluid mt-6 d-flex'>
+                <div className='col justify-content-lg-start'>
+                    <h2 className='fw-bold fs-1'>
+                        HOW WE BUILD
+                    </h2>
+                    <h2 className='fw-light fs-2'>
+                        WITH HYDRA VR?
+                        <img className="about-img hide" src="./img/arrow.svg" alt="arrow" style={{width: '228px', height: '100px', paddingLeft:'10%'}}/>                
+                    </h2>
+                </div>
+                <div className='col justify-content-lg-end d-flex algin-items-center'>
+                    <p>Vitae sapien pellentesque habitant morbi tristique senectus et netus et. Feugiat nibh sed pulvinar proin gravida hendrerit lectus. Mi sit amet mauris commodo quis imperdiet massa tincidunt nunc. Viverra aliquet eget sit amet tellus. Ornare lectus sit amet est placerat in. Lectus magna fringilla urna porttitor rhoncus vitae.</p>
+                </div>
+            </div>
+            <div className='row container-fluid mt-5 m-4'>
+                <div className='col flex-column'>
+                    <div className="buble d-flex justify-content-center align-items-center">
+                        <h2>01</h2>
+                    </div>
+                    <p className='buble-content d-inline-flex'>
+                        <img className="about-img hide" src="./img/arrow-small.svg" alt="arrow" style={{width: '57px', height: '57px'}}/>                
+                        3D Conception & Design
+                    </p>
+                </div>
+                <div className='col flex-column'>
+                    <div className="buble d-flex justify-content-center align-items-center">
+                        <h2>02</h2>
+                    </div>
+                    <p className='buble-content d-inline-flex'>
+                        <img className="about-img hide" src="./img/arrow-small.svg" alt="arrow" style={{width: '57px', height: '57px'}}/>                
+                        Interaction Design                    
+                    </p>
+                </div>
+                <div className='col flex-column'>
+                    <div className="buble d-flex justify-content-center align-items-center">
+                        <h2>03</h2>
+                    </div>
+                    <p className='buble-content d-inline-flex'>
+                        <img className="about-img hide" src="./img/arrow-small.svg" alt="arrow" style={{width: '57px', height: '57px'}}/>                
+                        VR World
+                        User Testing
+                    </p>
+                </div>
+                <div className='col flex-column'>
+                    <div className="buble d-flex justify-content-center align-items-center">
+                        <h2>04</h2>
+                    </div>
+                    <p className='buble-content d-inline-flex'>
+                        <img className="about-img hide" src="./img/arrow-small.svg" alt="arrow" style={{width: '57px', height: '57px'}}/>                
+                        Hydra VR Deploy
+                    </p>
+                </div>
+            </div>
+            <div className='form-container container-fluid mt-5 m-4'>
+                <div className='row'>
+                    <div className='col flex-column d-flex align-items-center fs-1 fw-light'>
+                        <h2>JOIN HYDRA</h2>
+                        <p>Let’s Build Your VR Experience</p>
+                    </div>
+                </div>
+                <Form onSubmit={handleSubmit} className='From'>
+                    <div className="row mb-3">
+                        <div className="col-md-6">
+                        <Form.Group controlId="nameInput">
+                            <Form.Control className='input' type="text" name="name" placeholder="First Name" onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                        <div className="col-md-6">
+                        <Form.Group controlId="surnameInput">
+                            <Form.Control className='input' type="text" name="surname" placeholder="Last Name" onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col-md-6">
+                        <Form.Group controlId="emailInput">
+                            <Form.Control className='input' type="email" name="email" placeholder="Email" onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                        <div className="col-md-6">
+                        <Form.Group controlId="phoneInput">
+                            <Form.Control className='input' type="tel" name="phone" placeholder="Phone Number" onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col-md-12">
+                        <Form.Group controlId="themeInput">
+                            <Form.Control className='input' type="text" name="theme" placeholder="Subject" onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                    </div>
+                    <div className="row mb-3">
+                        <div className="col-md-12">
+                        <Form.Group controlId="messageInput">
+                            <Form.Control className='input' as="textarea" name="message" rows={3} placeholder="Tell Us Something..." onChange={handleChange} />
+                        </Form.Group>
+                        </div>
+                    </div>
+                    <Button type="submit" className='filled-button button-submit text-black'>
+                        SEND TO HYDRA
+                    </Button>
+                </Form>
             </div>
         </div>
     );
